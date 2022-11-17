5ff8ab3d80f95c8354095531d783181c937f0aea
Rashed <rashed019r@gmail.com>
2022-11-17 14:46:21 +0300

    add

---
+++ exmaple/bin/teams.dart
@@ -0,0 +1,10 @@
+import 'dart:math';
+
+void main() {}
+
+class Team {
+  String? nameTeam;
+  String? members;
+  int idTeam = Random().nextInt(9999);
+
+}
---
+++ exmaple/test/exmaple_test.dart
@@ -0,0 +1,8 @@
+import 'package:exmaple/exmaple.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
